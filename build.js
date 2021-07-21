7a425528b74ecf7928935142803a5fb1a0ce0b4f
msoler75 <msoler75@yahoo.es>
2021-07-21 10:53:50 -0400

    first commit

---
+++ build.js
@@ -0,0 +1,5 @@
+console.log('Project building...');
+
+// ...
+
+console.log('done!!!');
---
+++ server.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const DEFAULT_PORT = 8080;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+app.get('/', (req, res) => {
+    return 'Server is running in ' + (PORT===8080?'default port':'.env PORT')+` ${PORT}`;
+});
+
+app.get('/env', (req, res) => {
+    res.send({ message: 'ENV:' + JSON.stringify(process.env) });
+});
+
+app.listen(PORT, () => {
+    console.log(`Application listening on port ${PORT}!`);
+});
